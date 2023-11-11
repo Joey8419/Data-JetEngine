904ea91c8caabcc8c520b8acc4e4334f75a9ab94
Joey8419 <jnelson297@gmail.com>
2023-11-11 17:39:05 -0500

    finished die_visuals

--- die_visual.py
+++ die_visual.py
@@ -9,11 +9,12 @@ die_1 = Die()
 die_2 = Die()
 
 # Create a D6.
-die = Die()
+die_1 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -30,7 +31,7 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+my_layout = Layout(title='Results of rolling a D6 and a D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
 
 print(frequencies)
